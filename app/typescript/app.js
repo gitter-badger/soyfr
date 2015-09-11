439fbcd40659f5aff2a1cde8043b94fcbc13db3c
Sebastian Zorn <s.zorn@voycer.com>
2015-09-11 19:50:29 +0200

    First steps with angular to show user-input

---
+++ app/typescript/app.js
@@ -0,0 +1,27 @@
+//Typescript
+/// <reference path="typings/angular2/angular2.d.ts" />
+import {Component,View, bootstrap, NgFor} from 'angular2/angular2';
+
+@Component({
+  selector: 'user-input'
+})
+
+@View({
+  directives: [NgFor],
+  templateUrl : 'views/user-input.html',
+})
+
+// Component controller
+class UserComponent {
+  username: string;
+
+  constructor() {},
+
+  selectUsername(name: string) {
+    this.username = name;
+
+    // TODO: Socket.io emit
+  }
+}
+
+bootstrap(UserComponent);
